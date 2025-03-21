74afd32369a2fe2417673119187715d3d156328e
Chandan2343-nef <durgawatid746@gmail.com>
2025-03-21 16:44:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import ForgotPassword from "./components/ForgotPassword";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <ForgotPassword />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
